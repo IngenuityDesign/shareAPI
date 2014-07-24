54a2a734735498fd825b7b93f9306913680e7250
Stephen Parente <stephen@ingenuitydesign.com>
2014-07-24 17:32:19 -0400

    I lied. Now it recurses properly

--- server/auth/access.js
+++ server/auth/access.js
@@ -10,12 +10,15 @@ function defaultConfig() {
     };
 }
 
+//this needs to get cached. It's a huge waste of time;
+
 function getDirectoryConfig(url) {
     return when.promise(function(resolve,reject,notify) {
         //we need to take this URL and iterate through it to find the latest share config. Otherwise, we use the default
-        var urlWithShare = url + path.sep + "share.json",
-            oldpath = "";
-        while (url.length > 1 && oldpath != url) {
+        var oldpath = "",
+            urlWithShare = url + path.sep + "share.json";
+        
+        do {
             if (fs.existsSync(urlWithShare)) {
                 //make sure it stops
                 oldpath = url;
@@ -27,9 +30,11 @@ function getDirectoryConfig(url) {
                 return; //get out of here
             } else {
                 oldpath = url;
-                url = path.dirname(url)
+                url = path.dirname(url);
+                urlWithShare = url + path.sep + "share.json";
             }
-        }
+        } while (url.length > 1 && oldpath != url);
+        
         resolve(defaultConfig());
   
     });
